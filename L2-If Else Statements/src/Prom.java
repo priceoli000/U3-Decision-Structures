0643465c6bae01930d878d9d2d1c989151076c24
priceoli000 <priceoli000@warren.k12.in.us>
2021-11-17 11:49:24 -0500

    Finished Lesson 2 Notes

--- L2-If Else Statements/src/Prom.java
+++ L2-If Else Statements/src/Prom.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Prom {
 
     /* In order to purchase a ticket to go to prom,
@@ -6,4 +8,29 @@ public class Prom {
      */
 
 
+    public static void main(String[] args) {
+
+        int gradeLevel = Integer.parseInt(JOptionPane.showInputDialog("What grade level are you in? (9, 10, 11, 12)"));
+        int discipline = Integer.parseInt(JOptionPane.showInputDialog("How many suspensions have you had?"));
+        int money = Integer.parseInt(JOptionPane.showInputDialog("About how much money do you have? Round down to the nearest $5"));
+
+        if(gradeLevel >= 11) {
+            if(discipline >= 1) {
+                System.out.println("You can't go to Prom. that's ok, you can't get stood up at Prom either. Loser");
+            }else{
+
+                if(money >= 40) {
+                    System.out.println("You can get a Prom ticket");
+                }else{
+                    System.out.println("You can't go to Prom.");
+                }
+            }
+
+        }else{
+
+            System.out.println("You can't go to Prom. that's ok, you can't get stood up at Prom either. Loser");
+
+        }
+
+    }
 }
--- L2-If Else Statements/src/ShippingCosts.java
+++ L2-If Else Statements/src/ShippingCosts.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class ShippingCosts {
 
     /*
@@ -21,6 +23,43 @@ public class ShippingCosts {
 
     As always, your program should include a method.
      */
+
+    public static void main(String[] args) {
+
+        double itemWeight = Double.parseDouble(JOptionPane.showInputDialog("What is the weight of the item?"));
+        int milesShipped = Integer.parseInt(JOptionPane.showInputDialog("How many miles are you shipping it?"));
+        double shippingRate;
+        double cost;
+
+        if(itemWeight >= 10) {
+            shippingRate = 4.80;
+        }else{
+            if(itemWeight >= 6) {
+                shippingRate = 3.70;
+            }else{
+                if(itemWeight >= 2) {
+                    shippingRate = 2.20;
+                }else{
+                    shippingRate = 1.10;
+                }
+            }
+
+        }
+        cost = cost(shippingRate, milesShipped);
+
+        JOptionPane.showMessageDialog(null, "The cost is: " + cost);
+    }
+
+    public static double cost(double rate, int miles){
+
+        int costMultiplier = miles/100;
+
+        if(costMultiplier == 0){
+            costMultiplier = 1;
+        }
+
+        return rate * costMultiplier;
+    }
 
 
 }
