313f5ce99a480dd52465c41667dd8325279ae23b
priceoli000 <priceoli000@warren.k12.in.us>
2021-11-17 12:16:47 -0500

    Merge Error Fix

--- L1-If Statements/src/Grades.java
+++ L1-If Statements/src/Grades.java
@@ -10,30 +10,23 @@ public class Grades {
         double percentage = read.nextDouble();
 
         if(percentage < 60){
-            System.out.println("Your grade is F" + grade);
+            System.out.println("Your grade is F");
         }
 
         if(percentage >= 60){
-            if(percentage < 70) {
-                System.out.println("Your grade is D" + grade);
-            }
+            System.out.println("Your grade is D");
         }
 
         if(percentage >= 70){
-            if(percentage < 80) {
-                System.out.println("Your grade is C" + grade);
-            }
+            System.out.println("Your grade is C");
         }
 
         if(percentage >= 80){
-            if(percentage < 90) {
-                System.out.println("Your grade is B" + grade);
-            }
+            System.out.println("Your grade is B");
         }
 
         if(percentage >= 90){
-            System.out.println("Your grade is A" + grade);
+            System.out.println("Your grade is A");
         }
-
     }
 }
